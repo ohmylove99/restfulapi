259d1afbddb685099a2dd2402f55d340acb4195f
ohmylove99 <lengfei9928@hotmail.com>
2017-05-18 19:10:18 +0800

    Add mongo restful api

--- restfulapi-mysql/dbconnection.js
+++ restfulapi-mysql/dbconnection.js
@@ -1,13 +1,10 @@
-var mysql=require('mysql');
+var mysql = require('mysql');
 var config = require('config');
 var dbConfig = config.get('dbConfig');
-var connection=mysql.createPool({
-
-host:dbConfig.host,
-user:'staffdb',
-password:'staffdb',
-database:dbConfig.dbName
-
-
+var connection = mysql.createPool({
+	host:dbConfig.host,
+	user:'staffdb',
+	password:'staffdb',
+	database:dbConfig.dbName
 });
 module.exports=connection;
